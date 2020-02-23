e754fc3f54d638d58931510c5ad5714c54630cae
unknown <karthi3259@gmail.com>
2020-02-23 23:34:14 +0530

    Updates

---
+++ precache-manifest.bdbed91cd55dd6571d2587cb815bbb1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7157963c778bd1579afd2ecafeb6fe38",
+    "url": "/getsetgotravels/index.html"
+  },
+  {
+    "revision": "9d16843251ec45f0e2cc",
+    "url": "/getsetgotravels/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d370f17d8cb9eb6ba7d6",
+    "url": "/getsetgotravels/static/js/2.34403c2c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/getsetgotravels/static/js/2.34403c2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d16843251ec45f0e2cc",
+    "url": "/getsetgotravels/static/js/main.0280dac0.chunk.js"
+  },
+  {
+    "revision": "65716a920d7105792778",
+    "url": "/getsetgotravels/static/js/runtime-main.ca7a8ac2.js"
+  }
+]);
